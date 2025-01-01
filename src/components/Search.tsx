661f3a1889bc2028857cf995d1da4535999b86f2
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2025-01-01 10:00:55 -0300

    puasar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
   loadUser: (serName: string) => Promise<void>
 }
 
-import { useState } from "react"
+import { useState, KeyboardEvent } from "react"
 
 import { BsSearch } from "react-icons/bs"
 
@@ -11,12 +11,18 @@ import classes from "./Search.module.css"
 const Search = ({loadUser}: SearchProps) => {
   const [UserName, setUserName] = useState("")
 
+  const handleKeyDown = ( e: KeyboardEvent ) => {
+    if (e.key === "Enter") {
+      loadUser(UserName)
+    }
+  }
+
   return (
     <div className={classes.search}>
         <h2>Busca por um usuário:</h2>
         <p>Conheça seu melhores reppsitórios</p>
         <div className={classes.search_container}>
-            <input type="text" placeholder="Digite o name do usuário" onChange={(e) => setUserName(e.target.value)}/>
+            <input type="text" placeholder="Digite o name do usuário" onChange={(e) => setUserName(e.target.value)} onKeyDown={handleKeyDown}/>
             <button onClick={() => loadUser(UserName)}>
                 <BsSearch/> 
             </button>
