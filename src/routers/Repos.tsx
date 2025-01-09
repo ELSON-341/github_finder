b20767a49c32da9a91cdf76b6fd7a59588d800ac
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2025-01-09 09:47:00 -0300

    error

--- src/routers/Repos.tsx
+++ src/routers/Repos.tsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import BackBtn from '../components/BackBtn'
+import Loader from '../components/Loader' 
+import Repo from '../components/Repo'
 
 import classes from './Repos.module.css'
+
 
 import { RepoProps } from '../types/repo'
 
 const Repos = () => {
     const { username } = useParams()
 
-    const [repos, setrepos] = useState<RepoProps[] | [] | null>(null)
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null)
     const [isLoading, setIsLoader] = useState(false)
 
     useEffect(() => {
@@ -23,16 +26,29 @@ const Repos = () => {
 
             setIsLoader(false)
 
-            console.log(data);
+            setRepos(data);
             
         }
-        loadRepos(username)
+        
+        if (username) {
+            loadRepos(username)
+        }
     }, [])
 
+    if (!repos && isLoading) return <Loader/>
+
     return (
-        <div>
+        <div className={classes.repos}>
             <BackBtn/>
-            Repos {username}
+            <h2>Explore os repositórios do usuário: {username}</h2>
+            {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+            {repos && repos.length > 0 && (
+                <div className={classes.repos_container}>
+                    {repos.map((repo: RepoProps) => (
+                        <Repo key={repo.name} {...repo}/>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
